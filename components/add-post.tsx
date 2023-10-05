bd8adb677edc359b720c4bd943984288e520b6d8
arashk yadegar <yadegar.arashk@gmail.com@>
2023-10-05 16:47:50 +0330

    tags in addpost model undefined fiexed

--- components/add-post.tsx
+++ components/add-post.tsx
@@ -107,8 +107,8 @@ export default function AddPost({props} : any)  {
             <button onClick={addTags} className="bg-green-600 inline px-2  rounded-md text-white">ثبت</button>
           </div>
           <ul className="flex gap-2 p-2 flex-wrap">
-            {addPostForm.tags.map((tag:any) => {
-                return <li   className="bg-gray-400  text-xs inline px-3 py-2 rounded-full text-white hover:text-gray-500 shadow-md shadow-gray-500">{tag}</li>
+            { addPostForm.tags.map((element: any) => {
+                      return         <li   className="bg-gray-400  text-xs inline px-3 py-2 rounded-full text-white hover:text-gray-500 shadow-md shadow-gray-500">{element}</li>
             })}
           </ul>
        
