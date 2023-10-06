74802e91b9d27ea8e2539a569a726f95972065f3
arashk yadegar <yadegar.arashk@gmail.com@>
2023-10-06 21:03:33 +0330

    i added service file for login

---
+++ services/loginService.ts
@@ -0,0 +1,12 @@
+export class LoginService { 
+  baseUrl: string = "http://localhost:8000/auth/login/";
+  async login(username: string ,password: string,remember: string): Promise<any> {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: username,password: password,remember: remember})
+    };
+
+   return await fetch(this.baseUrl,requestOptions);
+  }
+}
