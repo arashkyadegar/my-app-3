58abdd7d4242408b8419fab970822fdc67dd0e1c
arashk yadegar <yadegar.arashk@gmail.com@>
2023-10-28 04:08:12 +0330

    i refactored

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,11 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+
+    },
     extend: {
       colors: {
         'black-rgba': 'rgba(0, 0, 0, 0.54)',
